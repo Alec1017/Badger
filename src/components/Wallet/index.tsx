dca0ae7ae4c01a0f114475be2668ef73d5432d4d
Alec DiFederico <alecdifederico@gmail.com>
2021-04-27 17:34:26 -0400

    added styled component to wallet button

--- src/components/Wallet/index.tsx
+++ src/components/Wallet/index.tsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { useWeb3React } from '@web3-react/core'
 import { Web3Provider } from '@ethersproject/providers'
+import styled, { css } from 'styled-components'
 
 import { injectedConnector } from '../../connectors'
 
 
+const WalletButton = styled.button<{ primary?: boolean }>`
+    background: transparent;
+    border-radius: 3px;
+    border: 2px solid palevioletred;
+    color: palevioletred;
+    margin: 0.5em 1em;
+    padding: 0.25em 1em;
+
+    ${props => props.primary && css`
+        background: palevioletred;
+        color: white;
+    `}
+`
+
 const Wallet = () => {
     const { chainId, account, activate, deactivate, active } = useWeb3React<Web3Provider>()
 
-    const onClick = () => {
-        activate(injectedConnector)
-    }
+    const onClick = () => active ? deactivate() : activate(injectedConnector)
 
     return (
         <div>
             <div>ChainId: {chainId}</div>
             <div>Account: {account}</div>
-            {active ? (
-                <div>
-                    <div>✅ </div>
-                    <button type="button" onClick={()=> deactivate()}>disconnect</button>
-                </div>
-
-                ) : (
-                <button type="button" onClick={onClick}>Connect</button>
-            )}
+            <WalletButton onClick={onClick}>
+                {active ? 'Disconnect Wallet' : 'Connect Wallet'}
+            </WalletButton>
         </div>
     )
 }
